30ebc67036d160ce2d463b2bbab7a65eae52fe60
BrazilDust <geanoelias@gmail.com>
2026-04-21 22:11:51 -0300

    google search console verification

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="google-site-verification" content="_TMczQDE5OCG9qhqfveYdIG7g1t3wmHQJlfHNaw7o5U" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
