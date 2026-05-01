6a2e108bc5270868b150cf29521ce269cd5a4b3e
BrazilDust <geanoelias@gmail.com>
2026-05-01 08:55:14 -0300

    analytics: Google Analytics G-YJ242FGRF6

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,22 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-YJ242FGRF6"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-YJ242FGRF6');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
